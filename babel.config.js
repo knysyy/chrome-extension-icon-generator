7328c39a4e669b1b52a82a95cf8a8e460e00c76f
Luku <luku.3659@gmail.com>
2019-08-20 01:31:14 +0900

    テストを追加

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-    presets: [['@babel/preset-env']],
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current',
+                },
+            },
+        ],
+    ],
+    env: {
+        test: {
+            plugins: ['@babel/plugin-transform-runtime'],
+        },
+    },
 }
--- src/lib/iconHandler.spec.js
+++ src/lib/iconHandler.spec.js
@@ -1,5 +1,53 @@
-const iconHandler = require('./iconHandler')
+import fs from 'fs'
+import iconHandler from './iconHandler.js'
 
-test('sample test sample', () => {
-    expect(1).toBe(1)
+describe('iconHandler', () => {
+    // iconの削除
+    beforeAll(() => {
+        try {
+            if (fs.existsSync('./assets/icon16.png')) {
+                fs.unlinkSync('./assets/icon16.png')
+                fs.unlinkSync('./assets/icon48.png')
+                fs.unlinkSync('./assets/icon128.png')
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    })
+
+    test('./assets/test1.txt should be Error', async () => {
+        let cmdObj = {
+            iconPath: './assets/test1.txt',
+            outputPath: './assets',
+        }
+        await expect(iconHandler(cmdObj)).rejects.toThrow(
+            'ファイルタイプが不正です'
+        )
+    })
+
+    test('./assets/test2.txt should be Error', async () => {
+        let cmdObj = {
+            iconPath: './assets/test2.txt',
+            outputPath: './assets',
+        }
+        await expect(iconHandler(cmdObj)).rejects.toThrow(
+            'ファイルタイプが不正です'
+        )
+    })
+
+    test('./assets should be Error', async () => {
+        let cmdObj = {
+            iconPath: './assets.txt',
+            outputPath: './assets',
+        }
+        await expect(iconHandler(cmdObj)).rejects.toThrow('pathが不正です')
+    })
+
+    test('./assets/test.png should not be Error', async () => {
+        let cmdObj = {
+            iconPath: './assets/test.png',
+            outputPath: './assets',
+        }
+        await expect(iconHandler(cmdObj)).resolves.not.toThrow()
+    })
 })
